bccc8203bd0bf48009a46e49d995a2cdef752917
Leif Denby <leif@denby.eu>
2013-09-27 14:52:36 +0100

    NACA airfoil data and pip requirements

--- reference/tests/NACA_0012/__init__.py
+++ reference/tests/NACA_0012/__init__.py
@@ -1,12 +1,19 @@
 # coding: utf-8
 
 from matplotlib._png import read_png
-import matplotlib.pyplot as plot
+import os
 
-data = read_png('amick_flat_M0.70.png')
-plot.imshow(data, extent=[0.0, 1.0, .2, -1.])
-plot.axes().set_aspect(0.5)
+def add_data(plot, mach=0.4):
+    path = os.path.dirname(os.path.abspath(__file__))
+    data = read_png(os.path.join(path, 'amick_flat_M%1.2f.png' % mach))
+    plot.imshow(data, extent=[0.0, 1.0, .2, -1.])
+    return plot
 
-#plot.gca().invert_yaxis()
 
-plot.show()
+if __name__ == "__main__":
+    import matplotlib.pyplot as plot
+    plot.ion()
+    add_data(plot)
+    plot.axes().set_aspect(0.5)
+    plot.draw()
+    raw_input()
